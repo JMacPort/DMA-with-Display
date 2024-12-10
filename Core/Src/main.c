a24a711414f94a6031d34a61b3b1b775f9012236
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-12-10 16:30:51 -0500

    Cleaned up initializers and added more detailed comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -8,23 +8,15 @@ void USART_Init();
 void DMA_to_USART(const char*);
 
 int main() {
-
     USART_Init();
     DMA_Init();
 
-    for(volatile int i = 0; i < 60000; i++);
+    for(volatile int i = 0; i < 65000; i++);
 
-    DMA_to_USART("=== Starting Program ===\r\n");  // Clear marker
-    while(DMA1_Stream6->CR & (1 << 0));            // Wait for complete
-
-    DMA_to_USART("Attempt #15\r\n");
-    while(DMA1_Stream6->CR & (1 << 0));            // Wait for complete
-
+    DMA_to_USART("=== Starting Program ===\r\n");
+    DMA_to_USART("Attempt #26\r\n");
     DMA_to_USART("Message 1\r\n");
-    while(DMA1_Stream6->CR & (1 << 0));            // Wait for complete
-
     DMA_to_USART("Message 2\r\n");
-    while(DMA1_Stream6->CR & (1 << 0));            // Wait for complete
 
     while(1) {
     }
@@ -32,39 +24,35 @@ int main() {
 
 // Initialize DMA
 void DMA_Init() {
-	RCC -> AHB1ENR |= (1 << 21);
+	RCC -> AHB1ENR |= (1 << 21);										// DMA1 Clock
 
-	DMA1_Stream6 -> CR &= ~(1 << 0);
+	DMA1_Stream6 -> CR &= ~(1 << 0);									// Disables the stream and waits until ready
 	while(DMA1_Stream6 -> CR & (1 << 0));
 
-	DMA1_Stream6 -> CR &= ~(7 << 25);
+	DMA1_Stream6 -> CR &= ~(7 << 25);									// Clears and sets the channel to 4
 	DMA1_Stream6 -> CR |= (4 << 25);
 
-	DMA1_Stream6 -> CR |= (1 << 6);
+	DMA1_Stream6 -> CR |= (1 << 6);										// Sets direction to peripheral from memory
 
-	DMA1_Stream6 -> CR |= (1 << 10);
+	DMA1_Stream6 -> CR |= (1 << 10);									// Sets memory increment mode
 }
 
 // Initialize USART for basic debugging
 void USART_Init() {
-	RCC -> APB1ENR |= (1 << 17);
-	RCC -> AHB1ENR |= (1 << 0);
+	RCC -> APB1ENR |= (1 << 17);										// USART2 Clock
+	RCC -> AHB1ENR |= (1 << 0);											// GPIOA Clock
 
-	GPIOA -> MODER &= ~((3 << (2 * 3)) | (3 << (3 * 3)));
-	GPIOA -> MODER |= (2 << (2 * 2)) | (2 << (2 * 3));
+	GPIOA -> MODER &= ~((3 << (2 * 3)));								// Sets RX to alternate function
+	GPIOA -> MODER |= (2 << (2 * 2));
 
-	GPIOA -> AFR[0] &= ~((0xF << (4 * 2)) | (0xF << (4 * 3)));
-	GPIOA -> AFR[0] |= (7 << (4 * 2)) | (7 << (4 * 3));
+	GPIOA -> AFR[0] &= ~((0xF << (4 * 2)));								// Sets alternate function to USART2
+	GPIOA -> AFR[0] |= (7 << (4 * 2));
 
-	USART2 -> BRR = 0x0683;
-	USART2 -> CR1 |= (1 << 3);
-	USART2 -> CR3 |= (1 << 7);
+	USART2 -> BRR = 0x0683;												// 9600 Baud Rate
+	USART2 -> CR1 |= (1 << 3);											// Transmitter Enable
+	USART2 -> CR3 |= (1 << 7);											// DMA Enable
 
-	volatile uint8_t temp = USART2->DR;
-	temp = USART2->SR;
-	(void)temp;
-
-	USART2 -> CR1 |= (1 << 13);
+	USART2 -> CR1 |= (1 << 13);											// USART2 Enable
 }
 
 // Initialize ADC
@@ -75,19 +63,17 @@ void USART_Init() {
 
 // DMA buffer print to LCD
 void DMA_to_USART(const char* str) {
-    while(DMA1_Stream6->CR & (1 << 0));
+    while(DMA1_Stream6->CR & (1 << 0));									// Waits for stream to disable
 
-    if(DMA1->HISR & (1 << 21)) {
+    if(DMA1->HISR & (1 << 21)) {										// Checks if the transfer is complete; Extra protection
         DMA1->HIFCR |= (1 << 21);
     }
 
 	uint16_t len = strlen(str);
 
-	DMA1_Stream6 -> NDTR = len;
-	DMA1_Stream6 -> PAR = (uint32_t)&USART2 -> DR;
-	DMA1_Stream6 -> M0AR = (uint32_t)str;
+	DMA1_Stream6 -> NDTR = len;											// Sets number of data values to length of string
+	DMA1_Stream6 -> PAR = (uint32_t)&USART2 -> DR;						// Sets peripheral output address to USART2's data register
+	DMA1_Stream6 -> M0AR = (uint32_t)str;								// Sets input data to the string
 
-	DMA1_Stream6 -> CR |= (1 << 0);
-
-	while(DMA1_Stream6->CR & (1 << 0));
+	DMA1_Stream6 -> CR |= (1 << 0);										// Enables the stream
 }
